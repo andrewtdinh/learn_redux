028e11e02353c307334f70bf090eb8442c90fe72
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-10 21:56:13 -0700

    Make new component folder

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,4 +4,4 @@ import {render} from 'react-dom';
 // import css
 import css from './styles/style.styl';
 
-render(<p>Hello</p>, document.getElementById('root'));
+render(<p>Hello World!!</p>, document.getElementById('root'));
