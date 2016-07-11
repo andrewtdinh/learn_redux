15b96cb2f1f2d8500616ab73633438dce5cb35fb
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-10 22:15:06 -0700

    Render Main component in the main js file

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -7,4 +7,4 @@ import css from './styles/style.styl';
 // import components
 import Main from './components/Main';
 
-render(<p>Hello World!!</p>, document.getElementById('root'));
+render(<Main/>, document.getElementById('root'));
