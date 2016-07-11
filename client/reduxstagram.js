0edbba07bec91f17c8bbcaceb896081a578a90f8
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-10 22:13:45 -0700

    Import Main into the main js file to render

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,4 +4,7 @@ import {render} from 'react-dom';
 // import css
 import css from './styles/style.styl';
 
+// import components
+import Main from './components/Main';
+
 render(<p>Hello World!!</p>, document.getElementById('root'));
