056adaa82f435dcce262e17b3bf297d6aa6ad33c
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-11 19:14:50 -0700

    Change Single.js

--- client/components/Single.js
+++ client/components/Single.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Single = React.createClass({
   render(){
     return (
+      <div className="single-photo">
+        I'm a Single component
+      </div>
     )
   }
 })
