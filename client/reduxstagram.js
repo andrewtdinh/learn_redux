c5813b22d010ff48f1b8caad7eb628a16b85e66a
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-12 20:35:22 -0700

    Add router and routes

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -12,4 +12,13 @@ import PhotoGrid from './components/PhotoGrid';
 // import react router deps
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path='/view/:postId' component={Single}></Route>
+    </Route>
+  </Router>
+)
+
 render(<Main/>, document.getElementById('root'));
