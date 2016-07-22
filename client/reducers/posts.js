82f3a6021fbc0da3bf59b323814b219407a41925
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-21 18:51:51 -0700

    Add post reducer

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,3 +1,10 @@
 // Reducers take in two things:
 //    1. The action (info about what happened)
 //    2. Copy of the current state
+
+function posts(state = [], action){
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
