f0c2a2c26a8e05816616f18717ccf4cde2d662e9
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-21 19:13:58 -0700

    Create a rootReducer function and export it

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,3 +3,7 @@ import {routerReducer} from 'react-router-redux';
 
 import posts from './posts';
 import comments from './comments';
+
+const rootReducer = combineReducers({posts, comments, router: routerReducer});
+
+export default rootReducer;
