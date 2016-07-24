a2adf3506f624599f21044aa1207333859f60435
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-23 17:38:20 -0700

    Create mapStateToProps function

--- client/components/App.js
+++ client/components/App.js
@@ -2,3 +2,13 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as actionCreators from '../action/actionCreators';
 import Main from './Main';
+
+function mapStateToProps(state){
+  return {
+    posts: state.props,
+    comments: state.comments
+  }
+}
+
+
+const App = connect(mapStateToProps, mapDispatchToProps);
