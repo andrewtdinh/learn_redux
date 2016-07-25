0854dbc42427db358abd32a0e5e85bfdbd6ca692
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-24 22:10:56 -0700

    Complete photo.js

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Photo = React.createClass({
   render(){
     return (
-
+      <figure className='grid-figure'>
+        I am a photo!
+      </figure>
     )
   }
 })
