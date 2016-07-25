888a739c213fcf2a53e0d4d0999bb104a87a3193
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-24 22:14:49 -0700

    Loop over all the posts to display them on PhotoGrid

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -4,7 +4,7 @@ const PhotoGrid = React.createClass({
   render(){
     return (
       <div className="photo-grid">
-        I am a Photo Grid component
+        {this.props.posts.map((posts, i) => <Photo/>)}
       </div>
     )
   }
