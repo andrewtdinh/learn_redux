3c5c44b6a27a1ae6f769e8e8fecf855f6f467846
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-30 23:06:10 -0700

    Getting the index of the post

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,6 +4,9 @@ import Photo from './Photo';
 
 const Single = React.createClass({
   render(){
+    //index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    console.log(i);
     return (
       <div className="single-photo">
         I'm a Single component
