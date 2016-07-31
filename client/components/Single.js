a5189ef11f14d13f92f5c09c174cf12ea61213ef
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-30 23:09:54 -0700

    Also getting the post

--- client/components/Single.js
+++ client/components/Single.js
@@ -6,7 +6,8 @@ const Single = React.createClass({
   render(){
     //index of the post
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
-    console.log(i);
+    const post = this.props.posts[i];
+    console.log(post);
     return (
       <div className="single-photo">
         I'm a Single component
