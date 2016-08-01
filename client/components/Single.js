4d0dc128d83990203d63e5419f548b879464f451
Andrew Dinh <andrewdinh74@yahoo.com>
2016-08-01 00:01:39 -0700

    Refactor

--- client/components/Single.js
+++ client/components/Single.js
@@ -5,9 +5,10 @@ import Comments from '.Comments';
 const Single = React.createClass({
   render(){
     //index of the post
-    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const {postId} = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[i];
-    const postComments = this.props.comments[this.props.params.postId];
+    const postComments = this.props.comments[postId];
 
     return (
       <div className="single-photo">
