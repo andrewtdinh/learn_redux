f4b9c0600a034a2b623daac004b5704de177073e
Andrew Dinh <andrewdinh74@yahoo.com>
2016-08-01 01:08:55 -0700

    Create a function and print out comment in it for now

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComment(comment, i){
+    console.log(comment)
+  },
   render(){
     return (
       <div className='comment'>
-        I'm the comments!!!
+        {this.props.postComments.map(this.renderComment)}
       </div>
     )
   }
