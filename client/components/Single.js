ebc6b91fce6a12b35321dcfef70e56d720309c2f
Andrew Dinh <andrewdinh74@yahoo.com>
2016-07-31 23:51:26 -0700

    Get specific comments for the post only

--- client/components/Single.js
+++ client/components/Single.js
@@ -7,7 +7,7 @@ const Single = React.createClass({
     //index of the post
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
     const post = this.props.posts[i];
-    const postComments = this.props.comments;
+    const postComments = this.props.comments[this.props.params.postId];
 
     return (
       <div className="single-photo">
