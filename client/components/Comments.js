eb0612b7bc5dc534fc74dd867dde7838fb1b2309
Andrew Dinh <andrewdinh74@yahoo.com>
2016-08-02 18:20:11 -0700

    Returning an empty form component

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -16,6 +16,9 @@ const Comments = React.createClass({
     return (
       <div className='comments'>
         {this.props.postComments.map(this.renderComment)}
+        <form ref='commentForm' className='comment-form'>
+          
+        </form>
       </div>
     )
   }
