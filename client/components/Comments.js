b7075b019941330a9c35d94bb9025df61fc80fda
Andrew Dinh <andrewdinh74@yahoo.com>
2016-08-02 18:22:33 -0700

    Add two input fields

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -17,7 +17,8 @@ const Comments = React.createClass({
       <div className='comments'>
         {this.props.postComments.map(this.renderComment)}
         <form ref='commentForm' className='comment-form'>
-          
+          <input type="text" ref='author' placeholder='author'/>
+          <input type="text" ref='comment' placeholder='comment'/>
         </form>
       </div>
     )
