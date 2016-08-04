c3df7179b609f5669bb8fb34de82f0d33ebb19a5
Andrew Dinh <andrewdinh74@yahoo.com>
2016-08-03 23:20:48 -0700

    Returning the modified state in the comments reducer

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,17 @@
+
+function postComments(state = [], action){
+    return state;
+}
+
 function comments(state = [], action){
+  if (typeof action.postId !== 'undefined'){
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action),
+    }
+  }
   return state;
 }
 
