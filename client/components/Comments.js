6e159531354a09aefe305060efd52e9615b9be56
Andrew Dinh <andrewdinh74@yahoo.com>
2016-08-04 21:13:47 -0700

    Use the standard form reset instead of setting each input value to empty string

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -19,8 +19,7 @@ const Comments = React.createClass({
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
-    this.refs.author.value = '';
-    this.refs.comment.value = '';
+    this.refs.commentForm.reset();
 
   },
 
