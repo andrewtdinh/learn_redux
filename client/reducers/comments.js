c1b595d664917a0baf107379dd35d0fe176eac07
Andrew Dinh <andrewdinh74@yahoo.com>
2016-08-04 21:27:10 -0700

    Return new state without the deleted comment

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -8,7 +8,13 @@ function postComments(state = [], action){
         text: action.comment
       }]
     case 'REMOVE_COMMENT':
-      return state;
+      //return the new state without the deleted comment
+      return [
+        //from the start to the one we want to delete
+        ...state.slice(0, action.i),
+        //after the deleted one to the end
+        ...state.slice(action.i + 1)
+      ]
     default:
       return state;
   }
