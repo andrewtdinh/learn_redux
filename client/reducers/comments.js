ca1b5f2f1bd559a762b767830a8c6323377d9726
Andrew Dinh <andrewdinh74@yahoo.com>
2016-08-06 22:45:05 -0700

    Change to comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -7,7 +7,7 @@ function postComments(state = [], action){
         user: action.author,
         text: action.comment
       }]
-    case 'REMOVE_COMMENT':
+    case 'REMOVE_COMMENTS':
       //return the new state without the deleted comment
       return [
         //from the start to the one we want to delete
